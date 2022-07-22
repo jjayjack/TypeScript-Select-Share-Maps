56c823c55783c4a9de2d664d461e1fc310642e11
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-22 14:16:20 -0500

    Grabbing value from form

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-// Code goes here!
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  console.log(enteredAddress);
+
+  //   Send to API
+}
+
+form.addEventListener("submit", searchAddressHandler);
